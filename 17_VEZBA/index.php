1be894d39a2d0e157782b8c994b0210bb08bf358
stojanovicmilijana <stojanovicm1991@gmail.com>
2023-08-16 22:05:09 +0200

    niz_objekata

--- 17_VEZBA/index.php
+++ 17_VEZBA/index.php
@@ -2,7 +2,7 @@
 $brojStrana=[500,200,330,400,120];
 $cena=[5000,3500,1200,900,600];
 
-//prolazaj radimo sa for
+//prolazak 
 
 // for($i=0;$i<count($brojStrana);$i++)
 // foreach($brojStrana as $brS)
@@ -13,8 +13,9 @@ $cena=[5000,3500,1200,900,600];
 // $brS2=$brojStrana[2];
 //cena cija je stranica najskuplja
 function maxprosek($brojStrana,$cena){
-   //indeksirani niz mogu odma da postavim :
-     $max=$cena[0]/$brojStrana[0];
+   //indeksirani niz mogu odma da postavim : (mogu da stavim max=0, 
+   //posto mi je priroda zadatka uvek pozitvan broj i isti br elemenata stavicu odma ovako)
+        $max=$cena[0]/$brojStrana[0];
      //mogu da krenem i od prvog indeksa $i=1, ali nije greska ni $i=0
      for($i=0;$i<count($brojStrana);$i++){
       $t=$cena[$i]/$brojStrana[$i];
@@ -25,20 +26,25 @@ function maxprosek($brojStrana,$cena){
    return $max;
 
 } echo maxprosek($brojStrana,$cena);
-
+echo '<br>';
+////////////////////////////////////////////////////
+//Prolazak asoc niz
+//foreach (brojStrana as $k=>$v)? da li treba svaki elem da raylayem na kljc vrenost yavisi od prirode zad
 $brojStranaA=['knjiga1'=>500,'knjiga2'=>200,'knjiga3'=>330,'knjiga4'=>400,'knjiga5'=>120];
 $cenaA=['knjiga1'=>5000,'knjiga2'=>3500,'knjiga3'=>1200,'knjiga4'=>900,'knjiga5'=>600];
 
-function maxProsekA(){
+function maxProsekA($brojStranaA,$cenaA){
   $max=0;
-  foreach ($brojStranaA as $k =>$v){
-    $t = $cenaA[$k]/$v;
+  foreach ($brojStranaA as $k =>$v){ //500 pomocu kljuca dohvatan cenu
+    $t = $cenaA[$k]/$v; // 5000
     if($max<$t){
       $max=$t;
     }
   }
   return $max;
-}
+} echo maxProsekA($brojStranaA ,$cenaA);
+echo '<br>';
+////////////////////////////////////////////////////////////
 
 $nizKnjiga = [
   ['brojStrana' => 500, 'cena'=>5000],
@@ -48,15 +54,24 @@ $nizKnjiga = [
   ['brojStrana' => 120, 'cena'=>600],
 ];
 
-function maxProsekNizA(){
+function maxProsekNizA($nizKnjiga){
   $max=0;
   for($i=0; $i<count($nizKnjiga);$i++){
-    $t = $nizknjiga[$i]['cena']/$nizKnjiga[$i]['brojStrana'];
+    $t = $nizKnjiga[$i]['cena']/$nizKnjiga[$i]['brojStrana'];
     if($max<$t){
       $max=$t;
     }
   }
+  return $max;
+} echo maxProsekNizA($nizKnjiga);
+echo '<br>';
+
+///////////// 
+$dan = ['temperatura' => [8,5,-15,2,0]];
+for($i=0; $i<count($dan['temperatura']); $i++){
+    $t= $dan['temperatura'][$i];
 }
+
 
 // Napraviti klasu Knjiga koja od privatnih polja sadrži:
 // naslov
@@ -71,7 +86,7 @@ function maxProsekNizA(){
 // Metodu skupa koja ispituje da li je knjiga skupa
 // (ukoliko je cena veća od 8000)
 // Metodu dugackoime koja ispituje da li je ime autora dugačko (ukoliko je broj karaktera u imenu veći od 18)
-class Knjiga{
+/* class Knjiga{
     private $naslov;
     private $autor;
     private $god_izdanja;
@@ -79,6 +94,64 @@ class Knjiga{
     private $cena;
 
 }
+ */
+
+class Knjiga {
+  public $brojStrana;
+  public $cena;
+
+}
+
+$k1 = new Knjiga();
+$k1 -> broj_Strana = 500;
+$k1 -> $cena = 5000;
+
+$k2 = new Knjiga();
+$k2 -> broj_Strana = 200;
+$k2 -> $cena = 3500;
+
+$k3 = new Knjiga();
+$k3 -> broj_Strana = 330;
+$k3 -> $cena = 1200;
+
+$k4 = new Knjiga();
+$k4 -> broj_Strana = 400;
+$k4 -> $cena = 900;
+
+$k5 = new Knjiga();
+$k5 -> broj_Strana = 120;
+$k5 -> $cena = 600;
+
+
+$knjige = array($k1, $k2, $k3, $k4, $k5); //indeksirani niz objekata , moze se proci i foreach i for petljom,lakse je foreach petljom
+
+function maxProsekNiz($knjige){
+  $max=0;
+  ////////// prolazak kroz niz objekata foreach petljom
+  foreach ($knjige as $knjiga) {
+   $t = $knjiga ->cena / $knjiga ->broj_Strana;
+   if($max<$t){
+    $max=$t;
+   }
+  }
+  return $max;
+} echo maxProsekNiz($knjige);
+echo '<br>';
+/////////////////////////////////
+function maxProsekN($knjige){
+  $max=0;
+  ////////// prolazak kroz niz objekata FOR petljom
+  for ($i=0; $i<count($knjige); $i++) {
+    $knjiga = $knjige [$i];
+   $t = $knjiga['$i'] / $knjiga ->broj_Strana;
+   if($max<$t){
+    $max=$t;
+   }
+  }
+  return $max;
+} 
+echo maxProsekN($knjige);
+echo '<br>';
 
 
 ?>
