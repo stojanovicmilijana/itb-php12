9ae316142d468f52571f5abe25749b46f772d97e
Milijana Stojanović <stojanovicm1991@gmail.com>
2024-04-13 23:20:57 +0200

    SELECT_limit,where,order_by

--- 20_UVOD_U_BAZE/select.sql
+++ 20_UVOD_U_BAZE/select.sql
@@ -40,3 +40,116 @@ SELECT `tasks_id`, `title`, `description`
 FROM `customers`
 WHERE `priority` = 1
 AND `due_date` IS NOT NULL;
+
+
+-- Iz tabele customers, pročitati sve klijente:
+-- Čija je plata između 300 i 800,
+
+SELECT `name`,`adress`,`state`,`salary`
+FROM `customers`
+WHERE `salary` BETWEEN 300 AND 800;
+
+--čija je plata jednaka 500,600 ili 700
+SELECT `name`,`adress`,`state`,`salary`
+FROM `customers`
+WHERE `salary` = 500,
+OR `salary` = 600,
+OR `salary` = 700;
+
+
+--bolje rešenje
+SELECT `name`,`adress`,`state`,`salary`
+FROM `customers`
+WHERE `salary` IN(500,600,700);
+
+--polje čije je ime Ana,Bojana ili Vuk
+SELECT `name`,`adress`,`state`,`salary`
+FROM `customers`
+WHERE `name` IN('Ana', 'Bojana', 'Vuk');
+
+--polje cije ime pocinje na slovo B
+SELECT *
+FROM `customers`
+WHERE `name` LIKE 'B%';
+
+--polje cije ime sadrzi  slovo 'j'
+SELECT `name`,`adress`,`state`,`salary`
+FROM `customers`
+WHERE `name` LIKE '%j%';
+
+
+-- Koji su iz Srbije, Rumunije ili Bugarske,
+SELECT `name`,`adress`,`state`,`salary`
+FROM `customers`
+WHERE `state` IN('Srbija', 'Rumunija', 'Bugarska');
+
+
+-- Koju potiču iz zemlje koja počinje na slovo “S”.
+SELECT `name`,`adress`,`state`,`salary`
+FROM `customers`
+WHERE `state` LIKE 's%';
+
+
+--Limitiranje broja rezultata
+SELECT *
+FROM `customers`
+LIMIT 2;
+
+SELECT *
+FROM `customers`
+WHERE `name` LIKE 'B%'
+LIMIT 1;
+
+--Prikazi prva dva korisnika koji imaju dvocifren br poseta
+SELECT *
+FROM `customers`
+WHERE `number_of_visits` BETWEEN 10 AND 99
+LIMIT 2;
+
+
+--Sortiranje podataka
+SELECT *
+FROM `customers`
+ORDER BY `name`;
+
+--svi korisnici po godinama od najstarijeg ka najmladjem
+SELECT *
+FROM `customers`
+ORDER BY `age`DESC;
+
+--svi korisnici po godinama od najmladjih ka najstarijima
+--i po broju poseta od vise ka manjem-primenice se kod onih koji imaju istu vrednost prvog polja
+SELECT *
+FROM `customers`
+ORDER BY `age`ASC, `number_of_visits` DESC;
+
+--prikari prva dva korisnika sa najvecim brojem poseta, a ciji je broj poseta dvocifren(odredi korisnike sa najvecim brojem poseta pa prikazi prva dva)
+
+SELECT *
+FROM `customers`
+WHERE `number_of_visits` BETWEEN 10 AND 99
+ORDER BY `number_of_visits` DESC
+LIMIT 2;
+
+--prikazi korisnika koji ima najmanju platu koja je u opsegu izmedju 300 ili 500
+--ako ima vise ovakvih korisnika, prikazi onog cije je ime prvo u leksikografskom poretku
+SELECT *
+FROM `customers`
+WHERE `salary` BETWEEN 300 OR 500
+ORDER BY `salary`,`name`
+LIMIT 1;
+
+-- Iz tabele customers, pročitati sve klijente:
+-- Koji su iz Srbije a plata je 600,
+-- Čije ime počinje na S ili imaju manje od 30god.
+SELECT *
+FROM `customers`
+WHERE `state` = "Srbija" 
+AND `salary`=600;
+
+--FUNKCIJE OD SELECTA
+--PREBROJATI KOLIKO IMA KUPACA IZMEDJU 30 I 40 GODINA
+
+SELECT COUNT(`age`)
+FROM `customers`
+WHERE `age` BETWEEN 30 and 40;
